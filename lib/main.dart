a4bf4c5575e67a4198fed2b443223a78bbe22dfe
Precious Michael <Precious.michael2002@gmail.com>
2020-12-05 21:58:57 -0800

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,71 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  dynamic list(String text1, String text2, String text3) {
+    return Container(
+      margin: EdgeInsets.fromLTRB(15.0, 0.0, 10.0, 10.0),
+      child: Card(
+        margin: EdgeInsets.only(top: 30.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: EdgeInsets.fromLTRB(25.0, 10.0, 0.0, 0.0),
+              child: Text(
+                '$text1',
+                style: TextStyle(
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+            SizedBox(height: 8.0),
+            Container(
+              padding: EdgeInsets.only(left: 25.0),
+              child: Text(
+                '$text2',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20.0,
+                ),
+              ),
+            ),
+            SizedBox(height: 8.0),
+            Container(
+              padding: EdgeInsets.only(left: 25.0),
+              child: Text(
+                '$text3',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20.0,
+                ),
+              ),
+            ),
+            Card(
+              color: Colors.greenAccent.shade700,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0),
+              ),
+              margin: EdgeInsets.symmetric(vertical: 20.0, horizontal: 25.0),
+              child: ListTile(
+                title: Text(
+                  'JOIN GROUP',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -34,195 +99,11 @@ class MyApp extends StatelessWidget {
         ),
         body: ListView(
           children: [
-            Container(
-              margin: EdgeInsets.fromLTRB(15.0, 0.0, 10.0, 10.0),
-              child: Card(
-                margin: EdgeInsets.only(top: 30.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      padding: EdgeInsets.fromLTRB(25.0, 10.0, 0.0, 0.0),
-                      child: Text(
-                        'Presenters',
-                        style: TextStyle(
-                          fontSize: 30.0,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black,
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 8.0),
-                    Container(
-                      padding: EdgeInsets.only(left: 25.0),
-                      child: Text(
-                        'Presenters',
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 20.0,
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 8.0),
-                    Container(
-                      padding: EdgeInsets.only(left: 25.0),
-                      child: Text(
-                        'ABIA STATE * ABA NORTH',
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20.0,
-                        ),
-                      ),
-                    ),
-                    Card(
-                      color: Colors.greenAccent.shade700,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15.0),
-                      ),
-                      margin: EdgeInsets.symmetric(
-                          vertical: 20.0, horizontal: 25.0),
-                      child: ListTile(
-                        title: Text(
-                          'JOIN GROUP',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20.0,
-                          ),
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.fromLTRB(15.0, 0.0, 10.0, 10.0),
-              child: Card(
-                margin: EdgeInsets.only(top: 15.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      padding: EdgeInsets.fromLTRB(25.0, 10.0, 0.0, 0.0),
-                      child: Text(
-                        'OUK MOVEMENT ABA BRANCH',
-                        style: TextStyle(
-                          fontSize: 30.0,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black,
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 8.0),
-                    Container(
-                      padding: EdgeInsets.only(left: 25.0),
-                      child: Text(
-                        'OUK MOVEMENT ABA BRANCH',
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 20.0,
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 8.0),
-                    Container(
-                      padding: EdgeInsets.only(left: 25.0),
-                      child: Text(
-                        'ABIA STATE * AROCHUWU',
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                    Card(
-                      color: Colors.greenAccent.shade700,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15.0),
-                      ),
-                      margin: EdgeInsets.symmetric(
-                          vertical: 20.0, horizontal: 25.0),
-                      child: ListTile(
-                        title: Text(
-                          'JOIN GROUP',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20.0,
-                          ),
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.fromLTRB(15.0, 0.0, 10.0, 10.0),
-              child: Card(
-                margin: EdgeInsets.only(top: 20.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      padding: EdgeInsets.fromLTRB(25.0, 10.0, 0.0, 0.0),
-                      child: Text(
-                        'Imo state',
-                        style: TextStyle(
-                          fontSize: 30.0,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black,
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 8.0),
-                    Container(
-                      padding: EdgeInsets.only(left: 25.0),
-                      child: Text(
-                        'For all indigenes of imo state',
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 20.0,
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 8.0),
-                    Container(
-                      padding: EdgeInsets.only(left: 25.0),
-                      child: Text(
-                        'ABIA STATE * BENDE',
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                    Card(
-                      color: Colors.greenAccent.shade700,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15.0),
-                      ),
-                      margin: EdgeInsets.symmetric(
-                          vertical: 20.0, horizontal: 25.0),
-                      child: ListTile(
-                        title: Text(
-                          'JOIN GROUP',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20.0,
-                          ),
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
+            list('Presenters', 'Presenters', 'ABIA STATE* ABA NORTH'),
+            list('OUK MOVEMENT ABA BRANCH', 'OUK MOVEMENT ABA BRANCH',
+                'ABIA STATE* AROCHUWU'),
+            list('Imo State', 'For all indigenes of Imo state',
+                'ABIA STATE* BENDE'),
           ],
         ),
         floatingActionButton: FloatingActionButton(
